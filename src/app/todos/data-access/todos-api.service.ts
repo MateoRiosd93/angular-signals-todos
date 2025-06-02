6c1b5c0ee1438d186062857cae523db7ad83e5b9
marios <marios@bancolombia.com.co>
2025-06-02 16:10:34 -0500

    feat(store): Se desacopla el store con el signal del service.

---
+++ src/app/todos/data-access/todos-api.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { API_URL } from "../../tokens/api-url.token";
+import { Todo } from "../models/todo";
+
+@Injectable({ providedIn: 'root' })
+export class TodosService {
+    private readonly BASE_URL = inject(API_URL)
+    private readonly httpClient = inject(HttpClient)
+
+    getAllTodos() {
+        return this.httpClient.get<Todo[]>(`${this.BASE_URL}/todos`)
+    }
+
+    getDetailTodo(id: number) {
+        return this.httpClient.get<Todo>(`${this.BASE_URL}/todos/${id}`)
+    }
+
+    createTodo(todo: Todo) {
+        return this.httpClient.post(`${this.BASE_URL}/todos`, todo)
+    }
+
+    deleteTodo(id: number) {
+        return this.httpClient.delete(`${this.BASE_URL}/todos/${id}`)
+    }
+}
---
+++ src/app/todos/data-access/todos-store.service.ts
@@ -0,0 +1,51 @@
+import { computed, inject, Injectable, signal } from "@angular/core";
+import { TodosService } from "./todos-api.service";
+import { Todo } from "../models/todo";
+
+interface TodosState {
+    todos: Todo[]
+    loading: boolean
+    error: boolean
+}
+
+@Injectable({providedIn: 'root'})
+export class TodosStore {
+    private readonly todosService = inject(TodosService)
+
+    // Creamos nuestro signal para manejar el state de los todos
+    private readonly state = signal<TodosState>({
+        todos: [],
+        loading: false,
+        error: false
+    })
+
+    // Creamos computed signals para acceder a las propiedades del state
+    readonly todos = computed(() => this.state().todos)
+    readonly loading = computed(() => this.state().loading)
+    readonly error = computed(() => this.state().error)
+
+    // Acciones del store
+    getAllTodos(){
+        this.state.update(state => ({...state, loading: true}))
+
+        this.todosService.getAllTodos().subscribe({
+            next: todos => {
+                this.state.update(state => ({
+                    ...state,
+                    loading: false,
+                    todos
+                }))
+
+                console.log(todos);
+            },
+            error: error => {
+                this.state.update(state => ({
+                    ...state,
+                    error: true
+                }))
+
+                console.error(error)
+            }
+        })
+    }
+}
